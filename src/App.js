a99ab280f198c286335fe402518531fb9091ef79
Tucker Vassau <tuckv@hotmail.com>
2020-04-17 22:15:23 -0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Button from '@material-ui/core/Button';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+            <Button variant="contained" to="/" color="primary">
+              <Link to="/">About</Link>
+            </Button>
+            <Button variant="contained" to="/" color="primary">
+              <Link to="/contact">Contact</Link>
+            </Button>
+            <Button variant="contained" to="/" color="primary">
+              <Link to="/projects">Projects</Link>
+            </Button>
+        </nav>
+
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
