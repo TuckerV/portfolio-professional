0467018c2c9b47984b22ebc2f73999d0100b2e39
Tucker Vassau <tuckv@hotmail.com>
2020-04-19 00:46:29 -0600

    Updates

---
+++ precache-manifest.5b2065dc08aa593510ec760d044c6315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4d06e0d8fd0e9c96a2e72668a01570",
+    "url": "/portfolio-professional/index.html"
+  },
+  {
+    "revision": "2d4d0549b876a8babb8c",
+    "url": "/portfolio-professional/static/css/main.970bb5b6.chunk.css"
+  },
+  {
+    "revision": "86a1663f8a1611ff28f9",
+    "url": "/portfolio-professional/static/js/2.6b2ec015.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio-professional/static/js/2.6b2ec015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4d0549b876a8babb8c",
+    "url": "/portfolio-professional/static/js/main.47964280.chunk.js"
+  },
+  {
+    "revision": "ce68aeda97d51177dd7e",
+    "url": "/portfolio-professional/static/js/runtime-main.da966d18.js"
+  },
+  {
+    "revision": "7bb34648f6499fa53a9fc05ffd966382",
+    "url": "/portfolio-professional/static/media/resume.7bb34648.pdf"
+  }
+]);
